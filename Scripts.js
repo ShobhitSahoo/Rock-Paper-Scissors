5002ea05949388046914956e63ec27827b9d7745
Shobhit Preetam Sahoo <48983232+ShobhitSahoo@users.noreply.github.com>
2020-05-31 07:15:32 +0530

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -3,7 +3,7 @@ var playerSelection, botSelection, wins, losses, result, playing, playerName;
 playerName = window.prompt('Enter your name please: ')
 document.querySelector('.player').textContent = playerName;
 
-window.alert('Zoom out the screen for better experience! OR use desktop site option of chrome!');
+alert('Zoom out the screen for better experience! OR use desktop site option of chrome!');
 
 const arenaText = document.querySelector('.arena-text');
 const arena = document.querySelector('.arena');
